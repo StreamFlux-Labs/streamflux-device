bbdaef852b20ee27153a820909919a367d161e17
Duong Quoc Hoang <duong.quoc.hoang.work@gmail.com>
2026-01-04 13:43:26 +0700

    test: setup initial google test infrastructure

---
+++ tests/test_main.cpp
@@ -0,0 +1,11 @@
+// tests/test_main.cpp
+#include <gtest/gtest.h>
+
+// Một test case mẫu đơn giản
+TEST(DemoTest, CheckEquality) {
+    EXPECT_EQ(1 + 1, 2);
+}
+
+TEST(DemoTest, CheckBoolean) {
+    EXPECT_TRUE(true);
+}
